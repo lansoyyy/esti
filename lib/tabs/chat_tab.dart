906da20165b11590e8c3111aca5e55051578ba3f
Lans12345 <olanalans12345@gmail.com>
2024-11-09 17:18:24 +0800

    progress

--- lib/tabs/chat_tab.dart
+++ lib/tabs/chat_tab.dart
@@ -1,3 +1,4 @@
+import 'package:esti/utils/data.dart';
 import 'package:esti/widgets/drawer_widget.dart';
 import 'package:esti/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,38 @@ class ChatTab extends StatefulWidget {
 
 class _ChatTabState extends State<ChatTab> {
   final msg = TextEditingController();
+
+  List<Map<String, String>> chatMessages = [];
+
+  void _sendMessage() {
+    final message = msg.text.trim();
+    if (message.isNotEmpty) {
+      setState(() {
+        chatMessages.add({'sender': 'user', 'message': message});
+      });
+
+      _getResponse(message);
+
+      msg.clear();
+    }
+  }
+
+  void _getResponse(String question) {
+    String response =
+        'Sorry, I didn\'t understand the question. Please ask something else.';
+
+    for (var faq in faqData) {
+      if (faq['question']!.toLowerCase() == question.toLowerCase()) {
+        response = faq['answer']!;
+        break;
+      }
+    }
+
+    setState(() {
+      chatMessages.add({'sender': 'bot', 'message': response});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,61 +88,73 @@ class _ChatTabState extends State<ChatTab> {
           }),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            const CircleAvatar(
-              minRadius: 80,
-              maxRadius: 80,
-              backgroundImage: AssetImage(
-                'assets/images/avatar.jpg',
-              ),
-            ),
-            const Expanded(
-              child: SizedBox(
-                height: 20,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: msg,
-                      maxLines: 1,
-                      decoration: InputDecoration(
-                        hintText: 'Type your message...',
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 15),
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
-                        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: chatMessages.length,
+              itemBuilder: (context, index) {
+                final message = chatMessages[index];
+                final isUserMessage = message['sender'] == 'user';
+
+                return Align(
+                  alignment: isUserMessage
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    decoration: BoxDecoration(
+                      color:
+                          isUserMessage ? Colors.blue[100] : Colors.grey[300],
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Text(
+                      message['message']!,
+                      style: TextStyle(
+                        color: isUserMessage ? Colors.black : Colors.black87,
                       ),
                     ),
                   ),
-                  const SizedBox(width: 10),
-                  FloatingActionButton(
-                    onPressed: () async {},
-                    backgroundColor: Colors.blueAccent,
-                    child: const Icon(
-                      Icons.send,
-                      color: Colors.white,
-                    ),
-                  )
-                ],
-              ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: msg,
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      hintText: 'Type your question...',
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                FloatingActionButton(
+                  onPressed: _sendMessage,
+                  backgroundColor: Colors.blueAccent,
+                  child: const Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
