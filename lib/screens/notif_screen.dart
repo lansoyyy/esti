975ed5aa0b0137eced8c5e27abe07ffcdc742822
Lans12345 <olanalans12345@gmail.com>
2024-11-17 15:11:22 +0800

    added notif func

--- lib/screens/notif_screen.dart
+++ lib/screens/notif_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:esti/utils/const.dart';
 import 'package:esti/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -16,25 +18,48 @@ class NotifScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.amber[700],
       ),
-      body: Expanded(
-        child: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: const Icon(
-                Icons.notifications_active,
-                color: Colors.red,
-              ),
-              title: TextWidget(
-                text: 'ESTI has responded to your question!',
-                fontSize: 18,
-                fontFamily: 'Bold',
-                color: Colors.black,
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection(userId)
+              .where('sender', isEqualTo: 'bot')
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.error);
+              return const Center(child: Text('Error'));
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Center(
+                    child: CircularProgressIndicator(
+                  color: Colors.black,
+                )),
+              );
+            }
+
+            final data = snapshot.requireData;
+            return Expanded(
+              child: ListView.builder(
+                itemCount: data.docs.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: const Icon(
+                      Icons.notifications_active,
+                      color: Colors.red,
+                    ),
+                    title: TextWidget(
+                      text: 'ESTI has responded to your question!',
+                      fontSize: 18,
+                      fontFamily: 'Bold',
+                      color: Colors.black,
+                    ),
+                  );
+                },
               ),
             );
-          },
-        ),
-      ),
+          }),
     );
   }
 }
